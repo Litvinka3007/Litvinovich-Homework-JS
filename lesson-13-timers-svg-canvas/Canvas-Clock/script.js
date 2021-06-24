7e1dad7f54a7b04b76e16290413660ac1071e390
Angelina <angelina16161616@gmail.com>
2021-06-24 19:02:58 +0300

    Add the logic for the project

--- lesson-13-timers-svg-canvas/Canvas-Clock/script.js
+++ lesson-13-timers-svg-canvas/Canvas-Clock/script.js
@@ -1,183 +1,136 @@
 'use strict';
 
-var baseRadius = 500; // радиус циферблата
-var numbersBaseRadius = baseRadius / 2.5; // радиус оси цифр циферблата
-var hourHandLength = baseRadius / 4; // длина часовой стрелки
-var minuteHandLength = baseRadius / 3.5; // длина минутной стрелки
-var secondHandLength = baseRadius / 3; // длина секундной стрелки
-var circleRadius = baseRadius / 37.5; // радиус кругов с цифрами
-var numberInCircleSize = circleRadius * 2 - 8; // размер цифр
-
-var digitalClockWidth = baseRadius / 3.3; // ширина цифровых часов
-var digitalClockHeight = baseRadius / 12.5; // высота цифровых часов
-var digitalClockFont = (baseRadius / 23) + 'px'; // размер цифр на цифровых часах
-
-var dotSize = 6; // размер точки в центре часов
-//var wrapper = document.getElementById('wrapper');
-
-const canvas = document.querySelector('#clock');
-canvas.width = baseRadius;
-canvas.height = baseRadius;
+const canvas = document.getElementById('canvas');
 const base = canvas.getContext('2d');
+canvas.width = canvas.height = 600;
 
-setInterval(createClock, 1000);
+const baseRadius = canvas.width / 2;  // радиус canvas
+base.translate(baseRadius, baseRadius); // центр canvas
+const clockRadius = baseRadius * 0.95; // радиус циферблата
+const numbersRadius = baseRadius * 0.75; // радиус расположения цифр на циферблате
+const numCircleRadius = baseRadius * 0.11; // радиус кругов с цифрами
+const dotSize = baseRadius * 0.04; // размер точки в центре часов
+const fontSize = clockRadius * 0.1 + 'px Arial';// размер шрифта
+const digitalClockWidth = baseRadius / 1.5; // ширина цифровых часов
+const digitalClockHeight = baseRadius / 6; // высота цифровых часов
+
+setInterval(createClock, 0);
 
 // UI
 
 function createClock() {
-  createMainCircle();
-  createDigitalClock();
-  createDigitalNumbers();
   createClockFace();
-  createHourArrow(hourHandLength, 'black');
-  createMinuteArrow(minuteHandLength, 'white');
-  createSecondArrow(secondHandLength, 'red');
+  createDigitalClock();
   createDecorativeDot(dotSize);
-}
-
-function createMainCircle() {
-  base.fillStyle = '#FA6775';
-  base.strokeStyle = '#F52549';
-  base.lineWidth = '10';
-  base.beginPath();
-  base.arc(baseRadius / 2, baseRadius / 2, baseRadius / 2 - 5, 0,Math.PI * 2, false);
-  base.fill();
-  base.stroke();
+  updateTime();
 }
 
 function createClockFace() {
-  for (var number = 1; number <= 12; number++) {
-    var angle = number / 12 * Math.PI * 2;
-    var x = ((baseRadius - circleRadius + 6) / 2) + Math.round(Math.sin(angle) * numbersBaseRadius);
-    var y = ((baseRadius - circleRadius + 16) / 2) - Math.round(Math.cos(angle) * numbersBaseRadius);
-    createHourCircle(x, y, base);
-    createHourNumbers(x, y, number, base);
-  }
-}
-
-function createHourCircle(circleX, circleY) {
-  base.fillStyle = 'white'
-  base.strokeStyle = 'white';
-  base.lineWidth = '1';
+  base.save();
   base.beginPath();
-  base.arc( circleX + 10, circleY, circleRadius, 0, Math.PI * 2);
+  base.arc(0, 0, clockRadius, 0, 2 * Math.PI);
+  base.fillStyle = '#FA6775';
+  base.strokeStyle = '#F52549';
+  base.lineWidth = 15;
   base.fill();
   base.stroke();
+
+  for (let number = 1; number <= 12; number++) {
+    var angle = number / 12 * Math.PI * 2;
+    var x = ((clockRadius - numbersRadius) / 2 - dotSize * 2.5) + Math.round(Math.sin(angle) * numbersRadius);
+    var y = ((clockRadius - numbersRadius) / 2 - dotSize * 2.5) - Math.round(Math.cos(angle) * numbersRadius);
+    createHourCircle(x, y);
+    createHourNumbers(number, x, y);
+  }
+
+  base.restore();
 }
 
-function createHourNumbers(circleX, circleY, number) {
-  base.font = `${numberInCircleSize} sans-serif`;
+function createHourCircle(x, y) {
+  base.save();
+  base.fillStyle = 'white';
+  base.strokeStyle = '#F52549';
+  base.lineWidth = '1';
+  base.beginPath();
+  base.arc(x, y, numCircleRadius, 0, 2 * Math.PI);
+  base.fill();
+  base.stroke();
+  base.restore();
+}
+
+function createHourNumbers(number, x, y) {
+  base.save();
+  base.font = fontSize;
   base.fillStyle = 'black';
+  base.textBaseline = 'middle';
   base.textAlign = 'center';
-  base.fillText(number, circleX + 9.6, circleY + 1.5);
-}
-
-function createHourArrow(size, color) {
-  base.save();
-  base.translate(500, 500);
-  base.rotate(rotateHourHandle());
-  base.beginPath();
-  base.fillStyle = color;
-  base.fillRect(0,0, size, 4);
-  base.translate(-500, -500);
+  base.fillText(number, x, y);
   base.restore();
 }
 
-function createMinuteArrow(size, color) {
+function createArrow(angel, arrowWidth, arrowLength) {
   base.save();
-  base.translate(500, 500);
-  base.rotate(rotateMinuteHandle());
   base.beginPath();
-  base.fillStyle = color;
-  base.fillRect(0,0, size, 4);
-  base.translate(-500, -500);
-  base.restore();
-}
-
-function createSecondArrow(size, color) {
-  base.save();
-  base.translate(500, 500);
-  base.rotate(rotateSecondHandle());
-  base.beginPath();
-  base.fillStyle = color;
-  base.fillRect(0,0, size, 4);
-  base.translate(-500, -500);
+  base.strokeStyle = 'black';
+  base.lineWidth = arrowWidth;
+  base.lineCap = 'round';
+  base.moveTo(0, 0);
+  base.rotate(angel);
+  base.lineTo(0, -arrowLength);
+  base.stroke();
+  base.rotate(-angel);
   base.restore();
 }
 
 function createDecorativeDot(size) {
+  base.save();
   base.beginPath();
-  base.arc(baseRadius / 2, baseRadius / 2, size, 0, Math.PI * 2);
-  base.fillStyle = 'white';
+  base.arc(0, 0, size, 0, 2 * Math.PI);
+  base.fillStyle = 'black';
   base.fill();
+  base.restore();
 }
 
 function createDigitalClock() {
-  base.fillStyle = '#FEF3E2';
+  base.save();
   base.beginPath();
-  base.fillRect(baseRadius / 2 - digitalClockWidth / 2, baseRadius / 2 + digitalClockHeight * 2, digitalClockWidth, digitalClockHeight);
+  base.fillStyle = '#FEF3E2';
+  base.rect(-digitalClockWidth / 2, digitalClockHeight, digitalClockWidth, digitalClockHeight);
+  base.fill();
+  base.restore();
+  createDigitalNumbers();
 }
 
 function createDigitalNumbers() {
-  var updateDigitalTime = updateDigitalClock();
-  base.beginPath();
-  base.font = `${digitalClockFont} sans-serif`;
+  base.save();
+  base.font = fontSize;
   base.fillStyle = 'black';
   base.textAlign = 'center';
-  base.textBaseline='middle';
-  base.fillText(updateDigitalTime, baseRadius / 2, baseRadius / 2 + digitalClockHeight * 2.5)
+  base.textBaseline = 'middle';
+  base.fillText(updateDigitalClock(), 0, digitalClockHeight * 1.5);
+  base.restore();
 }
 
 // Logic
 
-function rotateHourHandle() {
-  var temp = new Date();
-  var hour = temp.getHours();
+function updateTime() {
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
 
-  var thisHourRotate = ((hour / 12) * 360 - 90) / (180 / Math.PI);
+  var thisHourRotate = (hour * Math.PI / 6 ) + (minute * Math.PI / (6 * 60));
+  createArrow(thisHourRotate, clockRadius * 0.025, clockRadius * 0.5);
 
-  return thisHourRotate;
-}
+  var thisMinuteRotate = (minute * Math.PI / 30);
+  createArrow(thisMinuteRotate, clockRadius * 0.015, clockRadius * 0.6);
 
-function rotateMinuteHandle() {
-  var temp = new Date();
-  var minute = temp.getMinutes();
-
-  var thisMinuteRotate = (( minute / 60 ) * 360 - 90) / (180 / Math.PI);
-
-  return thisMinuteRotate;
-}
-
-function rotateSecondHandle() {
-  var temp = new Date();
-  var second = temp.getSeconds();
-
-  var thisSecondRotate = ((second / 60) * 360 - 90) / (180 / Math.PI);
-
-  return thisSecondRotate;
-}
-
-function formatDateTime(dt) {
-  var hours = dt.getHours();
-  var minutes = dt.getMinutes();
-  var seconds = dt.getSeconds();
-
-  return converter(hours, 2) + ':' + converter(minutes, 2) + ':' + converter(seconds, 2);
-}
-
-function converter(val, length) {
-  var timeValue = val.toString();
-
-  while (timeValue.length < length) {
-    timeValue = '0' + timeValue;
-  }
-
-  return timeValue;
+  var thisSecondRotate = second * Math.PI / 30;
+  createArrow(thisSecondRotate, clockRadius * 0.008, clockRadius * 0.7);
 }
 
 function updateDigitalClock() {
-  var time = new Date();
-  var formatTime = formatDateTime(time);
-
-  return formatTime;
+  var now = new Date();
+  var time = now.toLocaleTimeString();
+  return time;
 }
