c48c6766ec9caa94cc76f807f3d3b85cf25b4f2d
Angelina <angelina16161616@gmail.com>
2021-07-12 02:30:12 +0300

    Add some changes to the file

--- lesson-19-mvc/localStorage.js
+++ lesson-19-mvc/localStorage.js
@@ -3,10 +3,10 @@
 function TLocalStorage(localStorageKey) {
   var self = this;
   var lsHash = {};
-  var hashFromStorage = localStorage.getItem(localStorageKey);
 
   self.Reset = function() {
     lsHash = {};
+    var hashFromStorage = localStorage.getItem(localStorageKey);
     if (hashFromStorage) {
       lsHash = JSON.parse(hashFromStorage);
     }
@@ -15,24 +15,6 @@ function TLocalStorage(localStorageKey) {
 
   self.addValue = function(key, value) {
     lsHash[key] = value;
-    localStorage.setItem(localStorageKey, JSON.stringify(lsHash));
-  };
-
-  self.getValue = function(key) { // Получаем из хранилища данные с указанным ключом
-    return lsHash[key];
-  };
-
-  self.deleteValue = function(key) {
-    delete lsHash[key];
-    localStorage.setItem(localStorageKey, JSON.stringify(lsHash));
-  };
-
-  self.getKeys = function() { // Получаем данные хранилища
-    return Object.keys(lsHash);
-  };
-
-  /*self.addValue = function(key, value) {
-    lsHash[key] = value;
     var hashFromStorage = localStorage.getItem(localStorageKey); // Получаем из LS сохранённые ранее данные
     if (hashFromStorage !== null) { // Если в LS что-то есть,
       localStorage.removeItem(localStorageKey); // то удаляем значение с указанным ключом
@@ -40,6 +22,10 @@ function TLocalStorage(localStorageKey) {
     } else {
       localStorage.setItem(localStorageKey, JSON.stringify(lsHash)); // Если в LS ничего нет, просто сохраняем новое значение с указанным ключом в формате JSON
     }
+  };
+
+  self.getValue = function(key) { // Получаем из хранилища данные с указанным ключом
+    return lsHash[key];
   };
 
   self.deleteValue = function(key) {
@@ -54,5 +40,9 @@ function TLocalStorage(localStorageKey) {
     } else {
       return false;
     }
-  };*/
+  };
+
+  self.getKeys = function() { // Получаем данные из хранилища
+    return Object.keys(lsHash);
+  };
 }
